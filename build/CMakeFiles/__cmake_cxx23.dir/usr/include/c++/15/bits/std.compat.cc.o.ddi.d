a66c24d41bf52a9eb788050a1fe28957f66a83bb
Ashwin Kishin Banwari <ashwinkbanwari@gmail.com>
2026-01-06 17:52:58 +0100

    set up basic structure

---
+++ build/CMakeFiles/__cmake_cxx23.dir/usr/include/c++/15/bits/std.compat.cc.o.ddi.d
@@ -0,0 +1,4 @@
+CMakeFiles/__cmake_cxx23.dir/usr/include/c++/15/bits/std.compat.cc.o.ddi: \
+  /usr/include/c++/15/bits/std.compat.cc \
+  /usr/sbin/../lib/gcc/x86_64-redhat-linux/15/../../../../include/c++/15/stdbit.h \
+  /usr/sbin/../lib/gcc/x86_64-redhat-linux/15/../../../../include/c++/15/stdckdint.h
---
+++ build/CMakeFiles/concurrency.dir/main.cpp.o.ddi.d
@@ -0,0 +1,2 @@
+CMakeFiles/concurrency.dir/main.cpp.o.ddi: \
+  /home/axe/Code/concurrency/main.cpp
